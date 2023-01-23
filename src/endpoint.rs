b7f3ce60fe31e0b5250229ce8869cef755273966
bergeramit <bergeramit@gmail.com>
2023-01-23 17:56:13 +0200

    Basic endpoints implmenetation

---
+++ src/endpoint.rs
@@ -0,0 +1,23 @@
+use std::io::{Write, Read};
+use std::net::{Ipv4Addr, SocketAddr};
+use std::net::{TcpListener, TcpStream};
+
+pub struct Endpoint {
+    stream: TcpStream,
+}
+
+impl Endpoint {
+    pub fn new(stream: TcpStream) -> Self {
+        return Endpoint {
+            stream
+        }
+    }
+    
+    pub fn write(&mut self, buf: &[u8]) -> Result<usize, std::io::Error> {
+        self.stream.write(buf)
+    }
+
+    pub fn read(&mut self, buf: &mut [u8]) -> Result<usize, std::io::Error> {
+        self.stream.read(buf)
+    }
+}
--- src/main.rs
+++ src/main.rs
@@ -1,25 +1,36 @@
 mod cmd_config;
+mod endpoint;
 
-use std::net::Ipv4Addr;
+use endpoint::{Endpoint};
+use std::net::{Ipv4Addr};
+use std::net::{TcpListener, TcpStream};
 use cmd_config::{Action, Args};
 use clap::Parser;
 
-fn run_server() {
-    println!("Running Server...")
+fn connect_server() -> Endpoint {
+    let listener = TcpListener::bind((Ipv4Addr::LOCALHOST, 0)).unwrap();
+    let binded_addr = listener.local_addr().unwrap();
+    println!("Listening on {:?}...", binded_addr);
+    println!("On peers machine run: clipbond connect {:?} {:?}", binded_addr.ip(), binded_addr.port());
+    let (stream, addr) = listener.accept().expect("No client found!");
+    println!("Accepted connection from: {:?}", addr);
+    Endpoint::new(stream)
 }
 
-fn run_client(server_ip: Ipv4Addr, server_port: u16) {
-    println!("Running Client (connect to: {server_ip}:{server_port})...")
+fn connect_client(server_ip: Ipv4Addr, server_port: u16) -> Endpoint {
+    println!("Running Client (connect to: {server_ip}:{server_port})...");
+    let stream = TcpStream::connect((server_ip, server_port)).expect("Couldn't connect to server");
+    println!("Connected!");
+    Endpoint::new(stream)
 }
 
 fn main() {
     let args = Args::parse();
-    match args.action {
-        Action::Server => {
-            run_server();
-        },
-        Action::Connect {server_ip, server_port} => {
-            run_client(server_ip, server_port)
-        },
+    let endpoint = match args.action {
+        Action::Server => {connect_server()},
+        Action::Connect {
+            server_ip,
+            server_port
+        } => {connect_client(server_ip, server_port)},
     };
 }
