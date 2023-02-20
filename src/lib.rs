ce30ad128d9042e4ff4bd6d9982e75d81e2f35e3
Amit Berger <bergeramit@gmail.com>
2023-02-20 16:00:52 +0200

    Fixed tests to actually test the newly formatted endpoint

--- src/lib.rs
+++ src/lib.rs
@@ -3,33 +3,28 @@ pub mod clipboard_session;
 
 #[cfg(test)]
 mod tests {
-    use std::net::SocketAddr;
-    use crate::endpoint::{Endpoint};
+    use crate::endpoint::{Endpoint, ConnectionInfo};
     use std::net::{Ipv4Addr};
-    use std::net::{TcpListener, TcpStream};
-    use std::thread;
-
-    fn _server_endpoint(listener: TcpListener) -> Endpoint {
-        let (stream, _) = listener.accept().expect("No client found!");
-        Endpoint::new_server(stream)
-    }
-
-    fn _client_endpoint(server_addr: SocketAddr) -> Endpoint {
-        let stream = TcpStream::connect(server_addr).expect("Couldn't connect to server");
-        Endpoint::new_client(stream)
-    }
+    use std::{thread, time};
 
     #[test]
     fn test_server_write_client_read() {
         let mut rec_buf: [u8; 4] = [0; 4];
-        let listener = TcpListener::bind((Ipv4Addr::LOCALHOST, 0)).unwrap();
-        let bound_addr = listener.local_addr().unwrap();
+        let server_ip = Ipv4Addr::LOCALHOST;
+        let server_port = 18344;
 
-        let server_handle = thread::spawn(|| {
-            let mut endpoint = _server_endpoint(listener);
+        let clone_server_ip = server_ip;
+        let clone_server_port = server_port;
+
+        let server_handle = thread::spawn( move || {
+            let mut endpoint = Endpoint::new(ConnectionInfo::Server { listening_ip: clone_server_ip, listening_port: clone_server_port });
+            endpoint.setup();
             endpoint.write(&[1, 3, 3, 7]).unwrap();
         });
-        let mut endpoint = _client_endpoint(bound_addr);
+
+        let mut endpoint = Endpoint::new(ConnectionInfo::Client { server_ip, server_port });
+        thread::sleep(time::Duration::from_millis(100));
+        endpoint.setup();
         server_handle.join().unwrap();
         endpoint.read(&mut rec_buf).unwrap();
         assert_eq!([1, 3, 3, 7], rec_buf)
@@ -38,14 +33,21 @@ mod tests {
     #[test]
     fn test_client_write_server_read() {
         let mut rec_buf: [u8; 4] = [0; 4];
-        let listener = TcpListener::bind((Ipv4Addr::LOCALHOST, 0)).unwrap();
-        let bound_addr = listener.local_addr().unwrap();
+        let server_ip = Ipv4Addr::LOCALHOST;
+        let server_port = 18312;
+
+        let clone_server_ip = server_ip;
+        let clone_server_port = server_port;
 
         let client_handle = thread::spawn(move || {
-            let mut endpoint = _client_endpoint(bound_addr);
+            let mut endpoint = Endpoint::new(ConnectionInfo::Client { server_ip, server_port });
+            thread::sleep(time::Duration::from_millis(100));
+            endpoint.setup();
             endpoint.write(&[1, 3, 3, 7]).unwrap();
         });
-        let mut endpoint = _server_endpoint(listener);
+
+        let mut endpoint = Endpoint::new(ConnectionInfo::Server { listening_ip: clone_server_ip, listening_port: clone_server_port });
+        endpoint.setup();
         client_handle.join().unwrap();
         endpoint.read(&mut rec_buf).unwrap();
         assert_eq!([1, 3, 3, 7], rec_buf)
