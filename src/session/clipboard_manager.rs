55142a3199634badf4fc804cacedca6adc4670a0
Amit Berger <bergeramit@gmail.com>
2023-02-20 19:35:37 +0200

    added agnostic impl

--- src/session/clipboard_manager.rs
+++ src/session/clipboard_manager.rs
@@ -1,24 +1,14 @@
-use cli_clipboard::{ClipboardProvider};
+use cli_clipboard::{ClipboardProvider, ClipboardContext};
 
 pub struct ClipboardManager {
-    #[cfg(any(target_os = "macos", target_os = "linux"))]
-    context: cli_clipboard::linux_clipboard::LinuxClipboardContext,
-
-    #[cfg(target_os = "windows")]
-    context: cli_clipboard::windows_clipboard::WindowsClipboardContext,
+    context: ClipboardContext
 }
 
 impl ClipboardManager {
     pub fn new() -> Self {
-        #[cfg(any(target_os = "macos", target_os = "linux"))]
-        return ClipboardManager {
-            context: cli_clipboard::linux_clipboard::LinuxClipboardContext::new().unwrap()
-        };
-
-        #[cfg(target_os = "windows")]
-        return ClipboardManager {
-            context: cli_clipboard::windows_clipboard::WindowsClipboardContext::new().unwrap()
-        };
+        return Self {
+            context: ClipboardContext::new().unwrap()
+        }
     }
 
     pub fn set_content(&mut self, content: String) {
